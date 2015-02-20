588928eb73cb6597fdbd661d0176a64fca47fd50
SAM HAINS <sdh@eml.cc>
2015-02-21 01:52:53 +1100

    added history

--- game.js
+++ game.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
     var performance = "";
     var lastDifference = 0;
     var guesses =[];
+    var tempArray = [];
+    var historyStr ="";
+    var historyPopover;
 
     //ends the game
     var gameOver = function(winOrLose){
@@ -29,6 +32,9 @@ $(document).ready(function(){
 
     //pop out interface for the answer
     $('.hint').popover();
+
+    //pop out interface with history
+    $('.history').popover();
 
     //add answer to the hint
     var popover = $('.hint').data('bs.popover');
@@ -57,11 +63,21 @@ $(document).ready(function(){
 
                 }
 
-                if(remaining==5){
+
+                if(remaining==4){
+                    $('.history').show();
+                    
+
+                    //add answer to the hint
+                   
+
+
 
                 }
                 else{
 
+
+                    
                     if(Math.abs(difference)<Math.abs(lastDifference)){
                         //warmer
                         $('.getting-warmer').show();
@@ -81,24 +97,30 @@ $(document).ready(function(){
 
                     if(difference>15){
                         $('.panel-title').text("TOO HIGH! COLD ! !");
+                        tempArray.push('cold');
 
                     }
                     else if (difference<15 && difference>0){
                         $('.panel-title').text("-TOO HIGH! HOT");
+                        tempArray.push('hot');
                     }
                     else if(difference<-15){
                         $('.panel-title').text("COLD! TOO LOW !");
+                        tempArray.push('cold');
                     }
                     else if (difference>-15 && difference<0){
                         $('.panel-title').text("TOO LOW! HOT !");
+                        tempArray.push('hot');
                     }
 
+         
 
+                    historyStr += guesses[guesses.length -1]+":"+tempArray[tempArray.length -1]+" \n";
+                    historyPopover = $('.history').data('bs.popover');
+                    historyPopover.options.content = historyStr;
 
-
-                   
                     lastDifference = difference;
-                    
+
                     $('.col-xs-2').find('.remaining').text(remaining);
 
 
@@ -115,7 +137,10 @@ $(document).ready(function(){
                     location.reload();
                 });
             }
-            
+            else{
+                event.preventDefault();
+            }
+
 
 
    });
@@ -130,7 +155,7 @@ var testInput = function(guess, guesses){
         return false;
     }
 
-    if(num>0 && num<100){
+    if(num>0 && num<=100){
         return true;
     }
     else{
